60586ede4831656de8397055664633feca06af26
mahmudulhasanmotin <mahmudulhasanmotin@gmail.com>
2024-02-28 17:03:01 +0600

    portfolio compleate

--- js/script.js
+++ js/script.js
@@ -44,5 +44,25 @@ window.onscroll = () => {
 
 /*==================== scroll reveal ====================*/
 
+ScrollReveal({
+    // reset: true,
+    distance:'80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
 
 /*==================== typed js ====================*/
+// Frontend Developer !
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Tailwindcss Expert','Dreamer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+});
